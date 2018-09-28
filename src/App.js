1a88c31af4d61c23b57db46ee430573004e8b5a3
sinamirzahosseinian <sina.mirza2@gmail.com>
2018-09-29 00:04:17 +0330

    raw-template-added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Test from "./component/test";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Test />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/test.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {};
+  render() {
+    return <h1> this is working </h1>;
+  }
+}
+
+export default Test;
